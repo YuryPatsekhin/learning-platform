0ba39e7a04c9ba00144c70a7bfea5ed32cad0d4c
unknown <yura.potihin@gmail.com>
2020-09-25 00:14:34 +0300

    update login dialog

--- web/src/Components/Login/Login.js
+++ web/src/Components/Login/Login.js
@@ -6,9 +6,38 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogActions from '@material-ui/core/DialogActions';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
 import api from '~/Services/api';
 import { closeLoginDialog } from '~/Redux/Actions'
 import { connect } from "react-redux";
+
+const styles = {
+  contentWrapper: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  fields: {
+    display: 'block',
+    marginRight: '20px',
+  },
+  dialogText: {
+    width: '237px',
+  },
+  buttons: {
+    justifyContent: 'space-around',
+  },
+  dialogTitle: {
+    paddingBottom: '0px',
+  },
+  fieldsWrapper: {
+    marginBottom: '5px',
+  },
+  registerLink: {
+    cursor: 'pointer',
+    color: '#3f51b5',
+  }
+};
 
 class Login extends React.Component {
 
@@ -24,25 +53,29 @@ class Login extends React.Component {
     api.login(data);
   }
 
+  onRegisterClick = () => {
+    this.handleCancel();
+  }
+
   render() {
-    const { isLoginDialogOpen } = this.props;
+    const { isLoginDialogOpen, classes } = this.props;
 
     return (
-      <Dialog open={isLoginDialogOpen}>
-        <DialogTitle>Login</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Пожалуйста, авторизуйтесь или пройтиде регистрацию
-          </DialogContentText>
-          <TextField id="standard-basic" label="Login" />
-          <TextField id="standard-basic" label="Password" />
+      <Dialog onClose={this.handleCancel} open={isLoginDialogOpen}>
+        <DialogTitle className={classes.dialogTitle}>Login</DialogTitle>
+        <DialogContent className={classes.contentWrapper}>
+          <DialogContentText className={classes.dialogText}>Please sign in to your account or <span onClick={this.onRegisterClick} className={classes.registerLink}>register</span></DialogContentText>
+          <div className={classes.fieldsWrapper}>
+            <TextField className={classes.fields} id="standard-basic" label="Login" />
+            <TextField className={classes.fields} id="standard-basic" label="Password" />
+          </div>
         </DialogContent>
-        <DialogActions>
+        <DialogActions className={classes.buttons}>
           <Button onClick={this.handleCancel} color="primary">
             Cancel
           </Button>
           <Button onClick={this.handleSubscribe} color="primary">
-            Subscribe
+            Log in
           </Button>
         </DialogActions>
       </Dialog>
@@ -62,4 +95,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(Login));
