31f07dd3afb36f49f4e932c212a33813632247b8
unknown <yura.potihin@gmail.com>
2020-09-24 16:36:13 +0300

    add mongodb

--- server/server.js
+++ server/server.js
@@ -3,6 +3,19 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, function (err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  dbo.createCollection("customers", function (err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
+});
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
